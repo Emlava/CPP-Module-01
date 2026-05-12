1dd51e3fbec57df784d6623841e10ee3da92491c
Emilio Lara Vargas <emiliolaravargas@hotmail.com>
2026-05-12 21:00:15 +0200

    Working on ex00. Create the main with tests and write the Makefile.

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,10 @@
+#include "Zombie.hpp"
+
+// Method definitions
+
+void	Zombie::set_name(std:string new_name) { name = new_name; }
+
+void	Zombie::announce(void) { std::cout << name << ": BraiiiiiiinnnzzzZ..." << endl; }
+
+// Desctructor
+Zombie::~Zombie(void) { std::cout << name << " is done for good." << endl; }
---
+++ ex00/Zombie.hpp
@@ -0,0 +1,24 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+# include <iostream>
+
+class Zombie
+{
+	private
+		std::string	name;
+	public
+		void	set_name(std::string new_name);
+		void	announce(void);
+		// Destructor
+		~Zombie(void);
+};
+
+/**** newZombie.cpp ****/
+Zombie*	newZombie(std::string name);
+
+/**** randomChump.cpp ****/
+void	reandomChump(std::string name);
+
+#endif
