bd6a81c0c000b4a89da2c559525332f5cc313435
Emilio Lara Vargas <emiliolaravargas@hotmail.com>
2026-05-17 18:57:55 +0200

    Working on ex01.

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,21 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+# include <iostream>
+
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+		void	set_name(std::string new_name);
+		void	announce(void);
+		// Destructor
+		~Zombie(void);
+};
+
+/**** zombieHorde.cpp ****/
+Zombie*	zombieHorde(int N, std::string name);
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+int	main(void)
+{
+	int		N;
+	std::string	name;
+
+	while (name.empty())
+	{
+		std::cout << "Enter the number of zombies to be created: ";
+		// LEFT OFF HERE
+		// Use getline()
+	}
+}
