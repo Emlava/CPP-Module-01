3310b12c68626b7ea24fa4d787159f0336a19c3d
Emilio Lara Vargas <emiliolaravargas@hotmail.com>
2026-05-22 13:07:25 +0200

    Finished ex00.

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,25 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <string>
+# include <iostream>
+
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+		// Constructor
+		Zombie(std::string new_name);
+		void	announce(void);
+		// Destructor
+		~Zombie(void);
+};
+
+/**** newZombie.cpp ****/
+Zombie*	newZombie(std::string name);
+
+/**** randomChump.cpp ****/
+void	randomChump(std::string name);
+
+#endif
