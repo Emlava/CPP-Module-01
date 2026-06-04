e9316392af9db181aba74412dea5b697e6b53031
Emilio Lara <emiliolaravargas@hotmail.com>
2026-06-04 15:17:27 +0200

    Finished CPP Module 01.

---
+++ ex06/Harl.cpp
@@ -0,0 +1,51 @@
+#include "Harl.hpp"
+
+// Private methods
+void	Harl::debug(void)
+{
+		std::cout << "[ DEBUG ]\n";
+		std::cout << "This is a debugging message: diagnosis information would go here.\n\n";
+		return ;
+}
+
+void	Harl::info(void)
+{
+		std::cout << "[ INFO ]\n";
+		std::cout << "This is a message with extensive information: info would go here.\n\n";
+		return ;
+}
+
+void	Harl::warning(void)
+{
+		std::cout << "[ WARNING ]\n";
+		std::cout << "This is a message with a warning: be more careful next time.\n\n";
+		return ;
+}
+
+void	Harl::error(void)
+{
+		std::cout << "[ ERROR ]\n";
+		std::cout << "This is an error message: critical information would go here.\n\n";
+		return ;
+}
+
+// Public method
+void	Harl::complain(std::string level)
+{
+		static std::string	identifier_array[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+		static void	(Harl::*method_pointers[4])(void) =
+		{&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+		int	i = 0;
+
+		while (level != identifier_array[i])
+		{
+			if (++i > 3)
+			{
+				std::cout << "There's no function with the given name.\n";
+				return ;
+			}
+		}
+		(this->*method_pointers[i])();
+
+		return ;
+}
